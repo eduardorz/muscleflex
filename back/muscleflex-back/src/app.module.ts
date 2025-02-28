40edef386ddb8aff6d8afb1f2ac243354550019e
Eduardo Ruiz <ruizzmarin20@gmail.com>
2025-02-28 20:51:26 -0300

    initial back structure

--- back/muscleflex-back/src/app.module.ts
+++ back/muscleflex-back/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { ClassesModule } from './classes/classes.module';
+import { ClassessService } from './classess/classess.service';
+import { PlansModule } from './plans/plans.module';
+import { PaymentsModule } from './payments/payments.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AppointmentsModule, ClassesModule, PlansModule, PaymentsModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ClassessService],
 })
 export class AppModule {}
